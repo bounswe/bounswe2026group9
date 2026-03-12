a6b4999646e451e8455d01360be2ae2ac5815342
Muhittin Köybaşı <koybasimuhittin@gmail.com>
2026-03-12 13:20:23 +0300

    feat:add stub-app

---
+++ stub-app/config.js
@@ -0,0 +1,16 @@
+/**
+ * GLOBAL CONFIGURATION
+ *
+ * Change NUM_BUTTONS to control how many buttons appear on the homepage.
+ * Update BUTTON_LABELS to set the text displayed on each button.
+ */
+
+const NUM_BUTTONS = 5;
+
+const BUTTON_LABELS = [
+  "Button 1",
+  "Button 2",
+  "Button 3",
+  "Button 4",
+  "Button 5",
+];
---
+++ stub-app/handlers.js
@@ -0,0 +1,26 @@
+/**
+ * BUTTON CLICK HANDLERS
+ *
+ * Each button on the homepage has its own handler function below.
+ * Replace the placeholder console.log with your own implementation.
+ */
+
+function onButton1Click() {
+  console.log("Button 1 clicked -- implement me!");
+}
+
+function onButton2Click() {
+  console.log("Button 2 clicked -- implement me!");
+}
+
+function onButton3Click() {
+  console.log("Button 3 clicked -- implement me!");
+}
+
+function onButton4Click() {
+  console.log("Button 4 clicked -- implement me!");
+}
+
+function onButton5Click() {
+  console.log("Button 5 clicked -- implement me!");
+}
